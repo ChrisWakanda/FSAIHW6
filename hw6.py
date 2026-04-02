d1eaf981c821edc67f16407def55a70ff9ea2758
Aryan Raveshia <aryanraveshia@Aryans-MacBook-Pro.local>
2026-04-01 20:42:27 -0400

    hw6

---
+++ hw6.py
@@ -0,0 +1,148 @@
+import numpy as np
+from enum import Enum
+
+def solve_mdp():
+    """
+    Solve the 4x3 grid MDP using Value Iteration, TD Learning, and Q-Learning.
+    Returns:
+    utilities_vi: numpy array of shape (11,) - utilities from value iteration
+    policy: numpy array of shape (11,) - optimal policy from value iteration
+    utilities_td: numpy array of shape (11,) - utilities from TD learning
+    q_values: numpy array of shape (11, 4) - Q-values from Q-learning
+    """
+    #-----------------DO NOT MODIFY BELOW-----------------------#
+    # Define states and actions
+    states = [(0,0), (1,0), (2,0), (3,0), (0,1), (2,1), (3,1),
+    (0,2), (1,2), (2,2), (3,2)]
+    actions = ["NORTH", "EAST", "WEST", "SOUTH"]
+    # Terminal states
+    terminal_states = {(3,1): -1, (3,2): +1}
+    #-----------------DO NOT MODIFY ABOVE-----------------------#
+    
+    # Mapping string directons to (dx, dy) grid movements
+    class Directions(Enum):
+        NORTH=(0,1)
+        SOUTH=(0, -1)
+        WEST=(-1, 0)
+        EAST=(1, 0)
+    
+    # handles movement and detects wall collisions
+    def nextState(curr, direction):
+        oldx, oldy = curr
+        dirx, diry = Directions[direction].value
+        nextStatex=oldx+dirx
+        nextstatey=oldy+diry
+        if nextStatex < 0 or nextStatex > 3 or nextstatey < 0 or nextstatey > 2 or (nextStatex == 1 and nextstatey == 1):
+            return curr # return current state if invalid
+        return (oldx+dirx, oldy+diry)
+
+    utilities_vi=np.zeros(len(states))
+    store={}
+    for index, state in enumerate(states):
+        store[state]=index
+        
+    # The dictionary/map that stores the probabilties of the action shift
+    incline = {
+        "NORTH": [("NORTH", 0.8), ("EAST", 0.1), ("WEST", 0.1)],
+        "SOUTH": [("SOUTH", 0.8), ("EAST", 0.1), ("WEST", 0.1)],
+        "EAST":  [("EAST", 0.8), ("NORTH", 0.1), ("SOUTH", 0.1)],
+        "WEST":  [("WEST", 0.8), ("NORTH", 0.1), ("SOUTH", 0.1)]
+    }
+
+    # PART A - Value Iteration
+
+    policy=np.full(len(states), "", dtype=object)
+
+    for _ in range(100):
+        # Must update based on the previous iteration's board state
+        clonedUtils=np.copy(utilities_vi)
+        for state in states:
+            if state in terminal_states:
+                clonedUtils[store[state]]=terminal_states[state]
+                continue
+            baction=None
+            bval=float('-inf')
+            
+            #finds the action with the highest posible ultility value
+            for action in actions:
+                possibleVal=0
+                for dir, prob in incline[action]:
+                    nexts=nextState(state, dir)
+                    possibleVal += prob*utilities_vi[store[nexts]]
+                possibleVal*=0.9
+                if possibleVal>bval:
+                    bval=possibleVal
+                    baction=action
+            clonedUtils[store[state]]=bval
+            policy[store[state]]=baction
+        utilities_vi=clonedUtils
+
+    # PART B - TD Learning
+
+    alpha = 0.02
+    gamma = 0.9
+    utilities_td=np.zeros(len(states))
+    
+    for t_state in terminal_states:
+        utilities_td[store[t_state]] = terminal_states[t_state]
+
+    for _ in range(5000):
+        # dropping the agent anywhere for maximium exploration
+        curr = states[np.random.choice(len(states))]
+
+        while curr not in terminal_states:
+            nextAction=policy[store[curr]]
+            possibleDirections=[]
+            probabs=[]
+            for d, prob in incline[nextAction]:
+                possibleDirections.append(d)
+                probabs.append(prob)
+                
+            # random choice to see where the actual direction is and use that to find next state.
+            realDirection=np.random.choice(possibleDirections, p=probabs)
+            nexts=nextState(curr, realDirection)
+
+            # TD update rule
+            td_goal=0.0+gamma*utilities_td[store[nexts]]
+            td_error=td_goal - utilities_td[store[curr]]
+            utilities_td[store[curr]]+=alpha*td_error
+
+            curr=nexts
+
+    # PART C - Q Learning
+
+    q_values = np.zeros((len(states), 4))
+
+    for t_state in terminal_states:
+        for a in actions:
+            q_values[store[t_state], actions.index(a)] = terminal_states[t_state]
+
+    for _ in range(5000):
+        curr = states[np.random.choice(len(states))]
+        while curr not in terminal_states:
+            # pick a random action to learn the environment
+            action = np.random.choice(actions)
+            possibleDirections = []
+            probabs = []
+            for d, prob in incline[action]:
+                possibleDirections.append(d)
+                probabs.append(prob)
+            realDirection = np.random.choice(possibleDirections, p=probabs)
+            nexts = nextState(curr, realDirection)
+
+            # look ahead at the best possible future action
+            maxNextQ = np.max(q_values[store[nexts]])
+            q_goal=0.0+gamma*maxNextQ
+
+            currColumn = actions.index(action)
+            q_error = q_goal - q_values[store[curr], currColumn]
+            q_values[store[curr], currColumn] += alpha * q_error
+
+            curr = nexts
+
+    # rounding
+    utilities_vi=np.round(utilities_vi, 3)
+    utilities_td=np.round(utilities_td, 3)
+    q_values=np.round(q_values, 3)
+
+    return utilities_vi, policy, utilities_td, q_values
